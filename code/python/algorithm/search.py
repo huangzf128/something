9aa14812d7fabedfe2dab2ddc832e0fcf644cc6e
unknown <huangzf128@gmail.com>
2018-11-14 19:22:44 +0900

    search

--- code/python/algorithm/search.py
+++ code/python/algorithm/search.py
@@ -1,3 +1,5 @@
+import math
+
 def lineSearch(ary, target):
     for i, a in enumerate(ary, 0):
         if(a == target):
@@ -6,6 +8,7 @@ def lineSearch(ary, target):
     else:
         return -1
 
+# need sort
 def binarySearch(ary, target, start, end):
     index = (start + end) // 2
     if (ary[index] == target):
@@ -20,8 +23,27 @@ def binarySearch(ary, target, start, end):
     else:
         return binarySearch(ary, target, start, end)
 
+# need sort
+def jumpSearch(ary, target):
+    m = math.floor(math.sqrt(len(ary)))
+    curPlace = 0
+    for i in range(m):
+        curPlace = i
+        for blockNo in range(m):
+            curPlace += (blockNo * m)
+            if (ary[curPlace] == target):
+                return curPlace
+            elif (ary[curPlace] < target):
+                break
+            else:
+                if (blockNo == m - 1):
+                    return lineSearch(ary[curPlace + 1:], target)
+                else:
+                    return lineSearch(ary[curPlace + 1: (blockNo + 1) * m])
+
+
 if __name__ == "__main__":
     ary = ["1", "2", "3", "4", "5"]
 
     # lineSearch(ary, "2")
-    print(binarySearch(ary, "3", 0, len(ary)))
+#    print(binarySearch(ary, "3", 0, len(ary)))
