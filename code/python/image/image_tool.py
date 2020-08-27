2c631f66435bca301939f9c160b13ba4c48d7c43
williamking <huangzf128@gmail.com>
2020-08-27 23:47:02 +0900

    image crop

--- code/python/image/image_tool.py
+++ code/python/image/image_tool.py
@@ -10,6 +10,9 @@ class Image_Tool:
         self.root_dir = root_dir
         self.out_dir = out_dir
 
+        if not os.path.exists(self.out_dir):
+            os.makedirs(self.out_dir)
+
     def converter(self, img_name, to_type):
         img = Image.open(os.path.join(self.root_dir, img_name))
 
@@ -19,19 +22,23 @@ class Image_Tool:
             img.save(os.path.join(self.out_dir, os.path.splitext(img_name)[0] + ".jpg"), "JPEG", quality=95)
 
     def crop(self, img_name):
-        img = Image.open(os.path.join(self.root_dir, img_name))
-        img = img.crop((250, 0, img.size[0] - 250, img.size[1]))
+        img = Image.open(os.path.join(self.out_dir, img_name))
+        img = img.crop((260, 0, img.size[0] - 260, img.size[1]))
         img.save(os.path.join(self.out_dir, img_name), "JPEG", quality=95)
 
 
 if __name__ == '__main__':
 
-    img_tools = Image_Tool(os.path.join(os.getcwd(), "01"), os.path.join(os.getcwd(), "01"))
+    for i in range(1, 4):
+        no = str(i).zfill(1)
+        out_dir = os.path.join(os.getcwd(), "output/" + no)
+        img_tools = Image_Tool(os.path.join(os.getcwd(), no), out_dir)
+        img_tools_crop = Image_Tool(out_dir, out_dir)
 
-    # .pngファイルをリストで取得する
-    filepath_list = glob.glob(os.path.join(os.getcwd(), "01/*.png"))
-    for filepath in filepath_list:
-        # ファイルパスからファイル名を取得
-        file_name = os.path.basename(filepath)
-        img_tools.converter(file_name, JPG)
-        img_tools.crop(file_name.replace("png", "jpg"))
+        # .pngファイルをリストで取得する
+        filepath_list = glob.glob(os.path.join(os.getcwd(), no + "/*.png"))
+        for filepath in filepath_list:
+            # ファイルパスからファイル名を取得
+            file_name = os.path.basename(filepath)
+            img_tools.converter(file_name, JPG)
+            img_tools_crop.crop(file_name.replace(PNG, JPG))
